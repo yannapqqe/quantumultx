19bded41de59018804265d013ee675ad33d9bf4f
yannapqqe <142737977+yannapqqe@users.noreply.github.com>
2024-02-08 23:14:46 +0800

    Update cheese.js

---
+++ cheese.js
@@ -0,0 +1,14 @@
+[rewrite_local]
+
+^https?:\/\/rwapi.cheeseword\.com\/global url script-response-body 
+
+
+[mitm]
+
+hostname = rwapi.cheeseword.com
+
+var ojbk = JSON.parse($response.body);
+
+ojbk = {   "data" : {     "processAppleReceipt" : {       "error" : 0,       "subscription" : {         "productId" : "rwapi.cheeseword.com/v1/Vip/buy",         "originalTransactionId" : "570001184068302",         "tier" : "Vip",         "refundedDate" : null,         "refundedReason" : null,         "isInBillingRetryPeriod" : false,         "expirationDate" : "2999-09-09T09:09:09.000Z",         "gracePeriodExpiresAt" : null,         "overDeviceLimit" : false,         "expirationIntent" : "CUSTOMER_CANCELLED",         "__typename" : "AppStoreSubscription",         "user" : null,         "status" : "canceled",         "originalPurchaseDate" : "2022-09-09T09:09:09.000Z"       },       "__typename" : "SubscriptionResult"     }   } };
+
+$done({body : JSON.stringify(ojbk)});
