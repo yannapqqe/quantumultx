b89969256a9abedd3206f36af26e6ca565e1525b
yannapqqe <142737977+yannapqqe@users.noreply.github.com>
2024-02-09 11:34:08 +0800

    Update q.js

--- q.js
+++ q.js
@@ -4,5 +4,5 @@ hostname=notability.com
 
 
 
-^https:\/\/notability\.com\/subscriptions url script-response-body
+^https://notability.com/subscriptions url script-response-body
 https://raw.githubusercontent.com/yannapqqe/quantumultx/main/t.js
